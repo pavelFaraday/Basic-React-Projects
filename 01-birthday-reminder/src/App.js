9e165af02e99a56aeeb47c9fa2b2a92e9ab9f096
pavelFaraday <devstudio.corp@gmail.com>
2021-07-04 15:00:24 +0400

    01-Birthday-Reminder | setup all Props | map all properties

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const [people, setPeople] = useState(data);   {/* -- 1 -- */}
+
   return (
     <main>
       <section className="container">
-        <h3>0 Birthdays Today</h3>
-        <List />
+        <h3>Your {people.length} Friend have Birthdays Today</h3>  {/* -- 3 -- */}
+        <List people={people} />  {/* -- 2 -- */}
         <button onClick={()=> console.log('Testing!')}>Clear All</button>
       </section>
     </main>
@@ -14,3 +16,7 @@ function App() {
 }
 
 export default App;
+
+// 1. Initialize useState, set default value of state
+// 2. setup 'people' Prop ('people' Prop = 'people' initialized state => data[])
+// 3. {people.length} ---> number of objects in data file, that has an Array[]
