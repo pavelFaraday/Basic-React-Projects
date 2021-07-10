0d89e84743a9674d4b8680b2476e11a9e5502603
pavelFaraday <devstudio.corp@gmail.com>
2021-07-10 22:41:12 +0400

    finish 03-menu |  syncronize with data

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -3,11 +3,18 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+
 function App() {
   const [menuItems, SetMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState(allCategories)
 
   const filterItems = (category) => {
+    if (category === 'all') {
+      SetMenuItems(items)
+      return;
+    }
+
     const newItems = items.filter((item) => item.category === category);
     SetMenuItems(newItems);
   }
@@ -20,7 +27,7 @@ function App() {
           <div className="underline"></div>
         </div>
 
-        <Categories filterItems={filterItems} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
