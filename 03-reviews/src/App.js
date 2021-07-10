9176568a74b45ee241492aacc0375d76107237b6
pavelFaraday <devstudio.corp@gmail.com>
2021-07-10 13:33:27 +0400

    start 03-reviews  |  test REACT-ICONS

---
+++ 03-reviews/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Review from './Review';
+import { DiAndroid } from "react-icons/di";
+function App() {
+  return <h2>
+    < DiAndroid className='icon'/>
+  </h2>;
+}
+
+export default App;
