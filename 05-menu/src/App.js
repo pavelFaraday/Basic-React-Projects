2a235d5815d10a1da45e38533048557fd3e2e532
pavelFaraday <devstudio.corp@gmail.com>
2021-07-10 18:32:54 +0400

    Start 05-menu

---
+++ 05-menu/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems, SetMenuItems] = useState(items)
+  const [categories, setCategories] = useState([])
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        
+        <Categories />
+        <Menu />
+      </section>
+    </main>
+  )
+}
+
+export default App;
