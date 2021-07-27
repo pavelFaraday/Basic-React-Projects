0ea147510393bb4e44778d3c9e997deaec9d87d1
pavelFaraday <devstudio.corp@gmail.com>
2021-07-27 10:24:22 +0400

    09-color-generator | end input field logic

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -1,10 +1,48 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+// special library for generating tint&shade colors
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+	const [color, setColor] = useState("");
+	const [error, setError] = useState(false);
+	const [list, setList] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		try {
+			let colors = new Values(color).all(10);
+		} catch (error) {
+			setError(true);
+			console.log(error);
+		}
+	};
+
+	return (
+		<>
+			<section className="container">
+				<h3>color generator</h3>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						placeholder="#f12025"
+						className={`${error ? "error" : null}`}
+						value={color}
+						onChange={(e) => setColor(e.target.value)}
+					/>
+					<button type="submit" className="btn">
+						submit
+					</button>
+				</form>
+			</section>
+
+			<section className="colors">
+				<h4>Color Lists..</h4>
+			</section>
+		</>
+	);
 }
 
-export default App
+export default App;
