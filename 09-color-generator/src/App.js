b0e6b47546c6199c94cb529e0ce65d0cbd4a5471
pavelFaraday <devstudio.corp@gmail.com>
2021-07-28 11:22:46 +0400

    finish 09-color-generator

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
 import SingleColor from "./SingleColor";
 
-// special library for generating tint&shade colors
 import Values from "values.js";
 
 function App() {
 	const [color, setColor] = useState("");
 	const [error, setError] = useState(false);
-	const [list, setList] = useState([]);
+	const [list, setList] = useState(new Values("#f15025").all(10));
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-
 		try {
 			let colors = new Values(color).all(10);
+			setList(colors);
 		} catch (error) {
 			setError(true);
 			console.log(error);
@@ -27,19 +26,27 @@ function App() {
 				<form onSubmit={handleSubmit}>
 					<input
 						type="text"
-						placeholder="#f12025"
-						className={`${error ? "error" : null}`}
 						value={color}
 						onChange={(e) => setColor(e.target.value)}
+						placeholder="#f15025"
+						className={`${error ? "error" : null}`}
 					/>
-					<button type="submit" className="btn">
+					<button className="btn" type="submit">
 						submit
 					</button>
 				</form>
 			</section>
-
 			<section className="colors">
-				<h4>Color Lists..</h4>
+				{list.map((color, index) => {
+					return (
+						<SingleColor
+							key={index}
+							{...color}
+							index={index}
+							hexColor={color.hex}
+						/>
+					);
+				})}
 			</section>
 		</>
 	);
